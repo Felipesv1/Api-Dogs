f8c9771045aced48be26020692a831c6da976d33
Felipe Pereira <104787971+Felipesv1@users.noreply.github.com>
2022-09-12 16:34:55 -0300

    atualizado

--- src/Pages/DogPage/index.js
+++ src/Pages/DogPage/index.js
@@ -6,32 +6,38 @@ import styled from 'styled-components'
 
 
 
+
 export const Img = styled.img`
 
 max-width:400px;
 max-height:400px;
 border-radius:12px;
 
+
 `
 
 
 export const Container = styled.section`
+max-width:500px;
+height:100%;
 display:flex;
 justify-content:center;
 align-items:center;
 flex-direction:column;
-margin:100px auto;
+margin:180px auto;
+background:darkslateblue;
+  border-radius:15px;
+
 `
 
 export const BoxDOg = styled.div`
-width:550px;
-height:550px;
-background:darkslateblue;
+width:100%;
+height:500px;
 display:flex;
 justify-content:space-evenly;
 align-items:center;
 flex-direction:column;
-border-radius:15px;
+
 
 
 a{
